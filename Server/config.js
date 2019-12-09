4776931426120e1f2d04c7e1fe4f519fe2041a20
RameshPrabakar <39963912+RameshPrabakar@users.noreply.github.com>
2019-12-09 12:41:07 +0530

    Update config.js

--- Server/config.js
+++ Server/config.js
@@ -1,6 +1,6 @@
 var config = {
     'db_connection' : 'mongodb://192.168.1.13:27017/testing',
-    "dbConnection": "tcp://postgres:1q2w3e4r5t@localhost:5433/krds",
+    "dbConnection": "tcp://postgres:1q2w3e4r5t@localhost:5433/sampleDB",
     'port' : 8080
 }
 module.exports = config;
